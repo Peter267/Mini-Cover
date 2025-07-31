b4c2e4aa78aed335e985bd8ad544057fbf6ea9b7
𝙋𝙚𝙩𝙚𝙧𝟮𝟲𝟳 <wujingbo12@outlook.com>
2025-07-31 19:05:44 +0800

    添加字体，修改文本

--- src/config.js
+++ src/config.js
@@ -6,12 +6,18 @@ const fonts = [
     ['yozai', '悠哉字体', 'https://chinese-fonts-cdn.deno.dev/packages/yozai/dist/Yozai-Regular/result.css'],
     ['寒蝉全圆体', '寒蝉全圆体', 'https://chinese-fonts-cdn.deno.dev/packages/hcqyt/dist/ChillRoundFRegular/result.css'],
     ['Douyin Sans', '抖音美好体', 'https://chinese-fonts-cdn.deno.dev/packages/dymh/dist/DouyinSansBold/result.css'],
-    ['MaokenZhuyuanTi', '猫啃珠圆体', 'https://chinese-fonts-cdn.deno.dev/packages/mkzyt/dist/猫啃珠圆体/result.css']
+    ['MaokenZhuyuanTi', '猫啃珠圆体', 'https://chinese-fonts-cdn.deno.dev/packages/mkzyt/dist/猫啃珠圆体/result.css'],
+    ['LXGW WenKai', '霞鹜文楷', 'https://fontsapi.zeoseven.com/292/main/result.css'],
+    ['LXGW WenKai Screen', '霞鹜文楷屏幕阅读版', 'https://chinese-fonts-cdn.deno.dev/packages/lywkpmydb/dist/LXGWWenKaiScreen/result.css'],
+    ['LXGW WenKai Mono Screen', '霞鹜文楷屏幕阅读版 - 窄', 'https://fontsapi.zeoseven.com/292/main/result.css'],
+    ['STDongGuanTi', '上图东观体', 'https://fontsapi.zeoseven.com/488/main/result.css'],
+    ['Maple Mono NF CN', 'Maple Mono NF-CN', 'https://fontsapi.zeoseven.com/442/main/result.css'],
+    ['NanoOldSong-A', '纳米老宋', 'https://fontsapi.zeoseven.com/467/main/result.css']
 ];
 
 export const defaultConfig = {
-    text: '梦爱吃鱼',       // 默认文本
-    watermark: '@梦爱吃鱼', // 默认水印
+    text: 'Peter276',       // 默认文本
+    watermark: '', // 默认水印
     fontFamily: import.meta.env.VITE_APP_FONT_FAMILY,
     fontStyles: fonts.map(f => f[2]).filter(Boolean),
     fontOptions: fonts.map(([value, label]) => ({ value, label }))
